e4107931b4ea1f5b2b73789b993e1e56555b1ff2
Hana Park <ydjxlst@gmail.com>
2023-05-12 14:03:15 +0900

    헤더 속 리스트  요소 진입  경로 추가

--- client/src/Layouts/Header.js
+++ client/src/Layouts/Header.js
@@ -11,8 +11,8 @@ const Header = () => {
             <div className="Header_wrap">
                 <img src={logo} alt="logo"/>
                 <ul className="List_wrap">
-                    <li>프로젝트 조회</li>
-                    <li>개발자 관리</li>
+                    <li><Link to="/Check_Proj">프로젝트 조회</Link></li>
+                    <li><Link to="/employee">개발자 관리</Link></li>
                     <li>평가 조회</li>
                 </ul>
                 <button><Link to="/login">로그아웃</Link></button>
