6bc8c39032559f2d1a4e428e266117253768e84a
Hana Park <ydjxlst@gmail.com>
2023-05-28 13:04:53 +0900

    아이디, 비밀번호 변경 구현

--- client/src/Pages/ChangeID.js
+++ client/src/Pages/ChangeID.js
@@ -1,41 +1,58 @@
-import React ,{useState} from "react";
-import '../styles/style.css'
+import React, { useState } from "react";
+import axios from "axios";
+import "../styles/style.css";
+import { UserAtom } from "../recoil/UserAtom";
+import { useRecoilState } from "recoil";
+axios.defaults.withCredentials = true;
 
-const ChangeID = () => { 
-    const [ni, setNi] = useState("")
+const ChangeID = () => {
+  const [newId, setNewId] = useState("");
+  const [user, setUser] = useRecoilState(UserAtom);
+  const [cuur_id, setCuur_id] = useState(user.id);
 
-    const LoginFunc = (e) => {
-        e.preventDefault();
-    }
-    
-    const myArray = ['1', '2', '3', '4', '5'];
-    function checkDuplicates(array, value) {
-        return array.includes(value);
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    if (newId === "") {
+      alert("새로운 아이디를 입력해 주세요");
+    } else if (newId === user.id) {
+      alert("이전과 동일한 아이디입니다");
+    } else {
+      try {
+        const response = await axios.patch(
+          `http://localhost:3001/user/resetId`,
+          {
+            cuur_id: cuur_id,
+            newId: newId,
+            headers: {
+              "Content-type": "application/json",
+              withCredentials: true,
+            },
+          }
+        );
+        const responseData = response.data;
+        alert(responseData);
+      } catch (error) {
+        alert(error);
       }
+    }
+  };
 
-
-    // const realID = "hana"
-    // const realPW = "1111"
-    return(
-        <div>
-           <form className="login_wrap" onSubmit={LoginFunc}>
-                <button>현재 아이디: xxx</button>
-                <input type="text" 
-                placeholder="새로운 아이디"
-                value={ni}
-                onChange={(e) => setNi(e.target.value)} />
-                <button type="submit" onClick={e => {
-                    if(checkDuplicates(myArray,ni)) {
-                        alert("이미 사용 중인 아이디입니다.")
-                    }else if (ni === "") {
-                        alert("새로운 아이디를 입력해 주세요")
-                    }else {
-
-                        alert("아이디가 변경되었습니다.")
-                    }       
-                }}>아이디 변경 </button>
-            </form>
-        </div>
-    )
-}
-export default ChangeID
+  return (
+    <div>
+      <form className="login_wrap" onSubmit={handleSubmit}>
+        <div>현재 아이디: {user.id}</div>
+        <input
+          type="text"
+          placeholder="새로운 아이디"
+          name="newId"
+          value={newId}
+          onChange={(e) => setNewId(e.target.value)}
+        />
+        <button type="submit">
+          아이디 변경{" "}
+        </button>
+      </form>
+    </div>
+  );
+};
+export default ChangeID;
--- client/src/Pages/ChangePW.js
+++ client/src/Pages/ChangePW.js
@@ -1,41 +1,110 @@
 import React, { useState } from "react";
+import axios from "axios";
+axios.defaults.withCredentials = true;
 
 const ChangePW = () => {
   const [email, setEmail] = useState("");
-  const [number, setNumber] = useState("");
-  const [password, setPassword] = useState("");
+  const [verify_code, setVerify_code] = useState("");
+  const [currPassword, setCurrPassword] = useState("");
+  const [newPassword, setNewPassword] = useState("");
   const [checkPW, setCheckPW] = useState("");
 
-  const formFunc = (e) => {
+  const handleEmail = async (e) => {
     e.preventDefault();
+    try {
+      const response = await axios.post(
+        `http://localhost:3001/user/sendEmail`,
+        {
+          email: email,
+          headers: {
+            "Content-type": "application/json",
+            withCredentials: true,
+          },
+        }
+      );
+      alert(response.data)
+    } catch (error) {
+      alert(error)
+    }
+  };
+
+  const handleVerifyEmail = async (e) => {
+    e.preventDefault();
+    try {
+      const response = await axios.post(
+        `http://localhost:3001/user/verifyEmail`,
+        {
+          verify_code: verify_code,
+          headers: {
+            "Content-type": "application/json",
+            withCredentials: true,
+          },
+        }
+      );
+      alert(response.data)
+    } catch (error) {
+      alert(error)
+    }
+  };
+
+  const handleChangePW = async (e) => {
+    e.preventDefault();
+    try {
+      if (currPassword === "" && newPassword === "" && checkPW === "") {
+        alert("입력을 확인해 주세요");
+      } else if (newPassword !== checkPW) {
+        alert("새로 입력한 비밀번호가 일치하지 않습니다");
+      } else {
+      }
+      const response = await axios.patch(
+        `http://localhost:3001/user/resetPW`,
+        {
+          currPassword: currPassword,
+          newPassword: newPassword,
+          headers: {
+            "Content-type": "application/json",
+            withCredentials: true,
+          },
+        }
+      );
+      alert(response.data)
+    } catch (error) {
+      alert(error)
+    }
   };
   return (
     <div className="Change_PW_wrap">
       <div>
-        <form className="">
+        <form onSubmit={handleEmail}>
           <input
-            type="text"
+            type="email"
             placeholder="이메일"
             value={email}
             onChange={(e) => setEmail(e.target.value)}
           ></input>
-          <button onClick={formFunc}>인증번호 발송</button>
+          <button type="submit">인증번호 발송</button>
         </form>
-        <form>
+        <form onSubmit={handleVerifyEmail}>
           <input
             type="text"
             placeholder="인증 번호"
-            value={number}
-            onChange={(e) => setNumber(e.target.value)}
+            value={verify_code}
+            onChange={(e) => setVerify_code(e.target.value)}
           ></input>
-          <button onClick={formFunc}>인증번호 확인</button>
+          <button type="submit">인증번호 확인</button>
         </form>
-        <form onSubmit={formFunc}>
+        <form onSubmit={handleChangePW}>
+          <input
+            type="password"
+            placeholder="현재 비밀번호"
+            value={currPassword}
+            onChange={(e) => setCurrPassword(e.target.value)}
+          ></input>
           <input
             type="password"
             placeholder="새로운 비밀번호"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
+            value={newPassword}
+            onChange={(e) => setNewPassword(e.target.value)}
           ></input>
           <input
             type="password"
@@ -43,20 +112,7 @@ const ChangePW = () => {
             value={checkPW}
             onChange={(e) => setCheckPW(e.target.value)}
           ></input>
-          <button
-            type="submit"
-            onClick={(e) => {
-              if (password === "" && checkPW === "") {
-                alert("입력을 확인해 주세요");
-              } else if (password !== checkPW) {
-                alert("비밀번호가 일치하지 않습니다");
-              } else {
-                alert("변경 성공!");
-              }
-            }}
-          >
-            비밀번호 변경
-          </button>
+          <button type="submit">비밀번호 변경</button>
         </form>
       </div>
     </div>
