446688226b1929b7f7f257628747f1834096b3be
Hana Park <ydjxlst@gmail.com>
2023-05-28 13:09:49 +0900

    마이페이지 계정 정보 출력 구현

--- client/src/Pages/Mypage.js
+++ client/src/Pages/Mypage.js
@@ -1,4 +1,5 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 import "../styles/style.css";
 import profile from "../assets/profile.jpg";
 import { Link } from "react-router-dom";
@@ -6,17 +7,13 @@ import ModalComponent from "../Components/ModalComponent";
 import ProjComponent from "../Components/ProjComponent";
 import { useRecoilState } from "recoil";
 import { UserAtom } from "../recoil/UserAtom";
+axios.defaults.withCredentials = true;
 
-const user_data = {
-  dept_name: "박하나",
-  dept_id: 60201680,
-  job: "개발자",
-};
 
 const Mypage = () => {
 
-  const [empty] = useState([])
   const [user, setUser] = useRecoilState(UserAtom)
+  const [employee, setEmployee] = useState({});
   const [modalIsOpen, setModalIsOpen] = useState(false);
   const [selectedItemId, setSelectedItemId] = useState(null);
 
@@ -30,38 +27,49 @@ const Mypage = () => {
   };
 
   console.log(user)
-  return (
+
+  useEffect(() => {
+    const renderData = async () => {
+      try {
+        const response = await axios.get("http://localhost:3001/mypage");
+        setEmployee(response.data.employee[0])
+      } catch (error) {
+        console.log("error name", error);
+        // API 호출이 실패한 경우에도 적절한 상태 업데이트 수행
+      }
+    };
+    renderData();
+  }, []);
+    return (
     <div>
       <div className="User_wrap">
         <div className="Profile_wrap">
           <img src={profile} alt="Profile" />
           <div className="Userdata_wrap">
             <div>
-              {user_data.dept_name} ({user.id})
+              {employee.employee_name} ({employee.employee_id})
             </div>
-            <div>{user_data.job}</div>
+            <div>{employee.position}</div>
           </div>
         </div>
         <div className="Change_wrap">
           <button className="Btn_changeID">
-            <Link to="/ChangeID">아이디 변경</Link>
+            <Link to="/user/ChangeID">아이디 변경</Link>
           </button>
           <button className="Btn_changePW">
-            <Link to="/changePW">비밀번호 변경</Link>
+            <Link to="/user/changePW">비밀번호 변경</Link>
           </button>
         </div>
       </div>
       <div className="Proj_wrap">
-      <ProjComponent items={empty} projTitle="투입" openModal={openModal} />
-      <ProjComponent items={empty} projTitle="종료" openModal={openModal} />
-      <ProjComponent items={empty} projTitle="평균 평점" openModal={openModal} />
+      <ProjComponent projTitle="진행중" openModal={openModal} />
+      <ProjComponent projTitle="종료" openModal={openModal} />
       </div>
       <ModalComponent
         isOpen={modalIsOpen}
         onRequestClose={closeModal}
         contentLabel="프로젝트 상세 정보"
         selectedItemId={selectedItemId}
-        items={empty}
       />
     </div>
   );
